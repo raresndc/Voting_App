1d8cbc68fe1b62397cbba78da0d08dee83d9900c
Rares Nedelcu <rares.ndc@gmail.com>
2024-10-25 14:04:52 +0300

    initialized springboot for auth

--- auth/src/test/java/com/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/auth/AuthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
