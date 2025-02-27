a65f4d4f2e1ca42acaed56ebecd54bfa7e74a66b
Rares Nedelcu <rares.ndc@gmail.com>
2025-02-27 10:30:42 +0200

    modified forgotPass/step2 layout to accomodate larger otp

--- front/src/pages/auth/ForgotPass2.tsx
+++ front/src/pages/auth/ForgotPass2.tsx
@@ -69,12 +69,17 @@ export default function ForgotPass2() {
           <form className="otp-Form">
           
             <span className="mainHeading">Enter OTP</span>
-            <p className="otpSubheading">We have sent a verification code to your mobile number</p>
+            <p className="otpSubheading">We have sent a verification code to your email</p>
             <div className="inputContainer">
             <input required="required" maxlength="1" type="text" className="otp-input" id="otp-input1"/>
             <input required="required" maxlength="1" type="text" className="otp-input" id="otp-input2"/>
             <input required="required" maxlength="1" type="text" className="otp-input" id="otp-input3"/>
             <input required="required" maxlength="1" type="text" className="otp-input" id="otp-input4"/> 
+            <input required="required" maxlength="1" type="text" className="otp-input" id="otp-input5"/> 
+            <input required="required" maxlength="1" type="text" className="otp-input" id="otp-input6"/> 
+            <input required="required" maxlength="1" type="text" className="otp-input" id="otp-input7"/> 
+            <input required="required" maxlength="1" type="text" className="otp-input" id="otp-input8"/> 
+            <input required="required" maxlength="1" type="text" className="otp-input" id="otp-input9"/> 
             
             </div>
             <button className="verifyButton" type="submit" onClick={navigateForgot3}>Next step</button>
