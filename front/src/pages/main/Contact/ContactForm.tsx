77a0077a69f4e1c672a74f5c01a533d9d868ffc3
Rares Nedelcu <rares.ndc@gmail.com>
2025-02-28 15:11:23 +0200

    updated contact us page to v3 - formatted the labels and changed the background color

--- front/src/pages/main/Contact/ContactForm.tsx
+++ front/src/pages/main/Contact/ContactForm.tsx
@@ -5,7 +5,7 @@ import { PhoneIcon, EnvelopeIcon, ChatBubbleLeftIcon } from '@heroicons/react/24
 
 const ContactForm: React.FC = () => {
   return (
-    <div className="bg-gradient-to-tl from-[#0F172A] to-[#1E293B] py-24 px-8 md:px-12">
+    <div className="bg-white py-24 px-8 md:px-12">
       {/* Integrating the ContactHeader */}
       <ContactHeader />
 
@@ -27,23 +27,22 @@ const ContactForm: React.FC = () => {
           icon={<EnvelopeIcon className="w-6 h-6" />}
         />
 
-        <form className="flex flex-col gap-5 pb-5">
-          <div className="relative w-full">
-            <label htmlFor="name" className="absolute top-[-12px] left-2.5 bg-white px-2.5 text-[14px]">Name</label>
-            <input type="text" name="name" className="h-10 px-2" />
-          </div>
+<form className="flex flex-col gap-5 pb-5">
+  <div className="relative w-full">
+    <label htmlFor="name" className="absolute top-[-12px] left-2.5 bg-white px-2.5 text-[14px]">Name</label>
+    <input type="text" name="name" className="h-10 px-2 border border-black rounded" />
+  </div>
 
-          <div className="relative w-full">
-            <label htmlFor="email" className="absolute top-[-12px] left-2.5 bg-white px-2.5 text-[14px]">E-Mail</label>
-            <input type="email" name="email" className="h-10 px-2" />
-          </div>
+  <div className="relative w-full">
+    <label htmlFor="email" className="absolute top-[-12px] left-2.5 bg-white px-2.5 text-[14px]">E-Mail</label>
+    <input type="email" name="email" className="h-10 px-2 border border-black rounded" />
+  </div>
 
-          <div className="relative w-full">
-            <label htmlFor="text" className="absolute top-[-12px] left-2.5 bg-white px-2.5 text-[14px]">Text</label>
-            <textarea name="text" rows={5} className="px-2"></textarea>
-          </div>
-        </form>
-
+  <div className="relative w-full">
+    <label htmlFor="text" className="absolute top-[-12px] left-2.5 bg-white px-2.5 text-[14px]">Text</label>
+    <textarea name="text" rows={5} className="px-2 border border-black rounded"></textarea>
+  </div>
+</form>
         <div className="flex justify-end">
           <Button text="SUBMIT" />
         </div>
