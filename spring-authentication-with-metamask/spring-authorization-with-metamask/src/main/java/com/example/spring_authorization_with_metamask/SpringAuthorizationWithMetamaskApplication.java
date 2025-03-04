eefeffab8784676e164074f77692e75420b2cbcd
Rares Nedelcu <rares.ndc@gmail.com>
2025-03-04 13:27:18 +0200

    initialized proj

---
+++ spring-authentication-with-metamask/spring-authorization-with-metamask/src/main/java/com/example/spring_authorization_with_metamask/SpringAuthorizationWithMetamaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_authorization_with_metamask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorizationWithMetamaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthorizationWithMetamaskApplication.class, args);
+	}
+
+}
---
+++ spring-authentication-with-metamask/spring-authorization-with-metamask/src/test/java/com/example/spring_authorization_with_metamask/SpringAuthorizationWithMetamaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_authorization_with_metamask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationWithMetamaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
