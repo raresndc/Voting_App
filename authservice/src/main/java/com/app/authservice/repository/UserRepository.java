c33c1c0aaa967efc54cea6ca8a4cfff236795a22
User <rares.ndc@gmail.com>
2025-04-25 11:55:41 +0300

    jwt, access, refresh token and user document

---
+++ authservice/src/main/java/com/app/authservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.app.authservice.repository;
+
+import com.app.authservice.document.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
