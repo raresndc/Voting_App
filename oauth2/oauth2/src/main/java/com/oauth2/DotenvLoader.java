9bfcfbf7b60952cc084f28cfd489bd90ca509b79
raresndc <rares.ndc@gmail.com>
2025-04-27 14:15:13 +0300

    hide secrets

---
+++ oauth2/oauth2/src/main/java/com/oauth2/DotenvLoader.java
@@ -0,0 +1,16 @@
+package com.oauth2;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DotenvLoader {
+
+    public static void loadEnv() {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
+        System.setProperty("FACEBOOK_CLIENT_ID", dotenv.get("FACEBOOK_CLIENT_ID"));
+        System.setProperty("FACEBOOK_CLIENT_SECRET", dotenv.get("FACEBOOK_CLIENT_SECRET"));
+    }
+}
--- oauth2/oauth2/src/main/java/com/oauth2/Oauth2Application.java
+++ oauth2/oauth2/src/main/java/com/oauth2/Oauth2Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Oauth2Application {
 
 	public static void main(String[] args) {
+
+		DotenvLoader.loadEnv();
+
 		SpringApplication.run(Oauth2Application.class, args);
 	}
 
