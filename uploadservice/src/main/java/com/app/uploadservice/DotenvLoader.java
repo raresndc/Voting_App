de01573a6532254ed164124db06299305bb031a2
raresndc <rares.ndc@gmail.com>
2025-04-30 16:15:23 +0300

    added dotenv and new properties to mathc authservice

---
+++ uploadservice/src/main/java/com/app/uploadservice/DotenvLoader.java
@@ -0,0 +1,15 @@
+package com.app.uploadservice;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DotenvLoader {
+
+    public static void loadEnv() {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+        System.setProperty("APP_JWT_SECRET", dotenv.get("APP_JWT_SECRET"));
+    }
+}
--- uploadservice/src/main/java/com/app/uploadservice/UploadserviceApplication.java
+++ uploadservice/src/main/java/com/app/uploadservice/UploadserviceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UploadserviceApplication {
 
 	public static void main(String[] args) {
+		DotenvLoader.loadEnv();
+
 		SpringApplication.run(UploadserviceApplication.class, args);
 	}
 
