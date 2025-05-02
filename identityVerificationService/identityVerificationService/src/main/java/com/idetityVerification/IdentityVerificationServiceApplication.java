d3a044976ca4008888b64b4c32cf2494df69d55a
raresndc <rares.ndc@gmail.com>
2025-05-02 16:47:08 +0300

    initialized identity verification service

---
+++ identityVerificationService/identityVerificationService/src/main/java/com/idetityVerification/IdentityVerificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.idetityVerification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityVerificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityVerificationServiceApplication.class, args);
+	}
+
+}
---
+++ identityVerificationService/identityVerificationService/src/test/java/com/idetityVerification/IdentityVerificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idetityVerification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityVerificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
