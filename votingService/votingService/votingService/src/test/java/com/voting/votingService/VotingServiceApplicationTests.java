e45ddabbe72fcab0b04527d8d58e9a87b03287e2
raresndc <rares.ndc@gmail.com>
2025-07-05 16:51:27 +0300

    initialized springboot service for voting

---
+++ votingService/votingService/votingService/src/test/java/com/voting/votingService/VotingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voting.votingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
