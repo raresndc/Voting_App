e25d69770c7390428082ecb9ea3593ec0add9fdd
raresndc <rares.ndc@gmail.com>
2025-07-08 20:59:17 +0300

    first version of candidates page

--- frontend/src/pages/Candidates.jsx
+++ frontend/src/pages/Candidates.jsx
@@ -18,7 +18,7 @@ export default function Candidates() {
   }, []);
 
   return (
-    <div className="p-6 text-gray-900">
+    <div className="p-6 text-gray-900 text-center">
       <h1 className="text-3xl font-bold mb-4">Candidates</h1>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {candidates.map((c, idx) => (
